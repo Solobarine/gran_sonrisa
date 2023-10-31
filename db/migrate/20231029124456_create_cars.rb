af267c6a65d5dd00cdfd33e6f0cf24a83c030194
Solly <solobarine@gmail.com>
2023-10-31 12:21:23 +0100

    Update Create Cars Migration

--- db/migrate/20231029124456_create_cars.rb
+++ db/migrate/20231029124456_create_cars.rb
@@ -1,18 +1,17 @@
-
 # frozen_string_literal: true;
 
-# Migration to Create Car Model
+## Migration to Create Car Model
 
 class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
-      t.name null: false
-      t.manufacturer
-      t.model null: false
-      t.year
-      t.price
-      t.description
-      t.timestamps null: false
+      t.string :name
+      t.string :manufacturer
+      t.string :model
+      t.integer :year
+      t.string :price
+      t.text :description
+      t.timestamps
     end
   end
 end
