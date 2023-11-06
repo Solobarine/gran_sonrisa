0764c3f199cf44095b0f7930a2a589a5d9869d29
Solly <solobarine@gmail.com>
2023-11-06 11:15:35 +0100

    Add Validation to Model Fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,11 @@ class User < ApplicationRecord
   :trackable, :confirmable
 
   # Validation
-  validates :name, presence: true
-  validates :name, length: { minimum: 2 }
+  validates :first_name, presence: true
+  validates :first_name, length: { minimum: 2 }
+
+  validates :last_name, presence: true
+  validates :last_name, length: { minimum: 2 }
 
   validates :email, presence: true
   validates :email, uniqueness: true
