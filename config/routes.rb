09fd4477d98795f837b6d1e1cb7798d7a645125b
Solly <solobarine@gmail.com>
2023-11-22 23:13:38 +0100

    Configure Project Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'home#index'
+  devise_for :users
+  resources :cars
+  get '/categories', to: 'cars#categories'
+  get '/categories/:id', to: 'cars#category'
+  get '/profile', to: 'users#show'
+  patch '/profile', to: 'users#update'
+  get '/dashboard', to: 'users#dashboard'
+  post '/initialize', to: 'orders#initialize_transaction'
+  resources :manufacturers
+  resources :favourites, only: %i[index create destroy]
 end
