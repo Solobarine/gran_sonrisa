098ef82efdf1dd568e2d1b04e8ba859dfaffd585
Solly <solobarine@gmail.com>
2023-11-22 23:14:07 +0100

    Add Pins to Import Map Config

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "swiper", to: "https://ga.jspm.io/npm:swiper@11.0.4/swiper.mjs"
