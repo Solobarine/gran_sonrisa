40bf510815461f26fb22ca89c7cdf033291c28e5
Solly <solobarine@gmail.com>
2023-11-23 08:54:15 +0100

    Remove Secrets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -19,14 +19,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   domain: 'gmail.com',
-  #   user_name: 'barinesolomon194@gmail.com',
-  #   password: 'destructo47',
-  #   authentication: 'plain',
-  #   enable_starttls_auto: true,
-  #   openssl_verify_mode: 'none'
   # }
 
   # Enable server timing
@@ -34,13 +26,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
