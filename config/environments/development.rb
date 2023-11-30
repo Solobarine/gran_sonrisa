c154a27ac6f05444207f6963217036c7ed2b8a93
solly <solobarine@gmail.com>
2023-11-30 21:58:40 +0100

    Set Default Storage to Amazon

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,8 +18,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.smtp_settings = {
-  # }
 
   # Enable server timing
   config.server_timing = true
@@ -41,7 +39,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
