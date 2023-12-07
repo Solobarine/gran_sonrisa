8acf6c2fb00ad262c9c7e3ff1044b6c04cd0d331
solly <solobarine@gmail.com>
2023-12-07 15:55:50 +0100

    Add column with name "condition" to cars table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_050521) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_07_084833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_050521) do
     t.string "fuel_type"
     t.string "drive_train"
     t.boolean "sold", default: false
+    t.string "condition"
     t.index ["manufacturer_id"], name: "index_cars_on_manufacturer_id"
   end
 
